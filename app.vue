03e6bc1fb670a0c7e1a8f248275d1367e98fed20
DesignComb <designcomb2021@gmail.com>
2023-08-21 15:45:06 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { data, pending, error, refresh } = await useAsyncData(
+    'mountains',
+    () => $fetch('https://api.notion.com/v1/databases/5ad9018ec1a9408cbc7781197832c880/query',{
+      method:'POST',
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'Authorization': 'Bearer secret_8Ujk50jsgGBNH8x1D8pi3DKB1ppeIRHAs9XNdMCoyog'
+      }
+    })
+)
+</script>
+<template>
+  <div>
+    {{data}}
+    <NuxtWelcome />
+  </div>
+</template>
