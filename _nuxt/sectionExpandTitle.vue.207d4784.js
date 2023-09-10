1c2065842078f919470ddb8fed17f5883e3405e3
DesignComb <DesignComb@users.noreply.github.com>
2023-09-10 11:04:43 +0000

    deploy: e2d77f64e8a70bde6d4ea6f0c85957842ac05992

---
+++ _nuxt/sectionExpandTitle.vue.207d4784.js
@@ -0,0 +1,1 @@
+import{f as o,o as s,c as a,I as t,F as r,d as n}from"./entry.a8b64a44.js";const p=o({__name:"sectionExpandTitle",setup(l){return(e,c)=>(s(),a(r,null,[t(e.$slots,"enTitle",{},()=>[n("Headline")]),t(e.$slots,"title",{},()=>[n("標題")])],64))}});export{p as _};
